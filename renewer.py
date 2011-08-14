017264730d6d9225696a66e0a05fa8ec2e79abbb
André Ericson <de.ericson@gmail.com>
2011-08-14 00:42:33 -0300

    Script incluso(renewer.py)

---
+++ renewer.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python2
+# Andre Ericson
+import Unicapbr
+from optparse import OptionParser
+import sys
+
+
+def main():
+    parser = OptionParser(usage="./%prog -m MATRICULA -p SENHA -d DIAS", version="%prog 0.1")
+    parser.add_option('-d','--dias', dest='dias', help='Numero de dias faltando para realizar a renovacao')
+    parser.add_option('-m', '--matricula', dest='matricula')
+    parser.add_option('-p', '--password', dest='password', help='Senha da biblioteca') 
+    (options, args) = parser.parse_args()
+    if not options.dias or not options.matricula or not options.password:
+        parser.print_help()
+        sys.exit(1)
+    try:
+        l = Unicapbr.Library(options.matricula, options.password)
+    except AssertionError:
+        print 'Nao foi possivel fazer o login, provavelmente senha/mat invalida.'
+        sys.exit(1)
+    renovados = l.renew_all_old(options.dias) 
+    if renovados:
+        print 'Os seguintes titulos foram renovados:'
+        for i in renovados:
+            print i.title
+    else:
+        print ('Nenhum livro foi renovado. Livro(s) deve(m) estar reservado(s) ou '
+        'a data de devolucao ja esta entre 15-17 dias.')
+
+if __name__ == '__main__':
+    main()
